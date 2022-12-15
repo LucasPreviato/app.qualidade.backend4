30d7b20ef39d98dca50ccab25bfbb1f43b824d04
Lucas Previato <lucaspreviato@live.com>
2022-12-14 22:16:15 -0300

    feat(positions): was implemented services and dto for position category
    created method create, findAll, findOne, update and remove for PositionCategories and created dto
    input

--- src/position-categories/position-categories.service.ts
+++ src/position-categories/position-categories.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InputNotFoundException } from 'src/errors/input-not-found-exception';
 import { CreatePositionCategoryInput } from './dto/create-position-category.input';
 import { UpdatePositionCategoryInput } from './dto/update-position-category.input';
+import { PositionCategory } from './entities/position-category.entity';
+import { PositionCategoriesRepository } from './repositories/position-categories-repository';
 
 @Injectable()
 export class PositionCategoriesService {
-  create(createPositionCategoryInput: CreatePositionCategoryInput) {
-    return 'This action adds a new positionCategory';
+  constructor(
+    private positionCategoriesRepository: PositionCategoriesRepository,
+  ) {}
+  async create({
+    name,
+  }: CreatePositionCategoryInput): Promise<PositionCategory> {
+    return await this.positionCategoriesRepository.create({ name });
   }
 
-  findAll() {
-    return `This action returns all positionCategories`;
+  async findAll(): Promise<PositionCategory[]> {
+    return await this.positionCategoriesRepository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} positionCategory`;
+  async findOne(id: number): Promise<PositionCategory> {
+    const positionCategory = await this.positionCategoriesRepository.findOne(
+      id,
+    );
+    if (!positionCategory) {
+      throw new InputNotFoundException(id);
+    }
+    return positionCategory;
   }
 
-  update(id: number, updatePositionCategoryInput: UpdatePositionCategoryInput) {
-    return `This action updates a #${id} positionCategory`;
+  async update(
+    id: number,
+    updatePositionCategoryInput: UpdatePositionCategoryInput,
+  ): Promise<PositionCategory> {
+    return await this.positionCategoriesRepository.update(
+      id,
+      updatePositionCategoryInput,
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} positionCategory`;
+  async remove(id: number): Promise<PositionCategory> {
+    return await this.positionCategoriesRepository.remove(id);
   }
 }
