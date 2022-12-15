741439ae6e7386f7380bcfe8243016a22753d09b
Lucas Previato <lucaspreviato@live.com>
2022-12-14 21:40:51 -0300

    feat(position): was implemented position category entity

--- src/position-categories/entities/position-category.entity.ts
+++ src/position-categories/entities/position-category.entity.ts
@@ -1,7 +1,18 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, OmitType } from '@nestjs/graphql';
 
 @ObjectType()
 export class PositionCategory {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  id: number;
+  @Field(() => String)
+  name: string;
+  @Field(() => Date)
+  createdAt: Date;
+  @Field()
+  positions: string;
 }
+
+@ObjectType()
+export class ResolvePositionCategories extends OmitType(PositionCategory, [
+  'createdAt',
+] as const) {}
