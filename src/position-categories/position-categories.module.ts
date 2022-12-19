3348d8897cfa8afe3aebf1d5792d267e3cfc2cc5
Lucas Previato <lucaspreviato@live.com>
2022-12-19 20:27:53 -0300

    feat(position-category): was implement provide and useclass for acesse repository

--- src/position-categories/position-categories.module.ts
+++ src/position-categories/position-categories.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PositionCategoriesService } from './position-categories.service';
 import { PositionCategoriesResolver } from './position-categories.resolver';
+import { PositionCategoriesRepository } from './repositories/position-categories-repository';
+import { PrismaPositionCategoriesRepository } from './repositories/prisma/prisma-position-categories-repository';
 
 @Module({
-  providers: [PositionCategoriesResolver, PositionCategoriesService]
+  providers: [
+    PositionCategoriesResolver,
+    PositionCategoriesService,
+    {
+      provide: PositionCategoriesRepository,
+      useClass: PrismaPositionCategoriesRepository,
+    },
+  ],
 })
 export class PositionCategoriesModule {}
