7c2d901b1679392d507889d5bcc744a0dd70e362
Miguel Aguena <miguel.aguena737@gmail.com>
2022-12-21 22:50:37 -0300

    feat(started document entity): started making the entity for document

--- src/documents/entities/document.entity.ts
+++ src/documents/entities/document.entity.ts
@@ -1,7 +1,73 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql'
+import {
+  ObjectType,
+  Field,
+  Int,
+  registerEnumType,
+  OmitType,
+} from '@nestjs/graphql'
+import { Status } from 'src/documents/enums/documents.enums'
+import { ResolveCollaborators } from 'src/collaborators/entities/collaborator.entity'
+import { ResolveUnit } from 'src/units/entities/unit.entity'
+import { ResolveDepartments } from 'src/departments/entities/department.entity'
+
+registerEnumType(Status, { name: 'Status' })
 
 @ObjectType()
 export class Document {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+  @Field(() => Int)
+  id: number
+
+  @Field(() => String)
+  name: string
+
+  @Field(() => String, { nullable: true })
+  reference: string
+
+  @Field(() => Date)
+  createdAt: Date
+
+  @Field(() => Date, { nullable: true })
+  updatedAt: Date
+
+  @Field(() => String, { nullable: true })
+  fileURL: string
+
+  @Field(() => String, { nullable: true })
+  pdfURL: string
+
+  @Field(() => ResolveCollaborators)
+  elaborator: ResolveCollaborators
+
+  @Field(() => Date, { nullable: true })
+  elaboratorAt: Date
+
+  @Field(() => ResolveCollaborators)
+  revisor: ResolveCollaborators
+
+  @Field(() => Date, { nullable: true })
+  revisorAt: Date
+
+  @Field(() => ResolveCollaborators)
+  approver: ResolveCollaborators
+
+  @Field(() => Date, { nullable: true })
+  approverAt: Date
+
+  @Field(() => Status, { defaultValue: 'ELABORATION' })
+  status: Status
+
+  @Field(() => ResolveUnit)
+  unit: ResolveUnit
+
+  @Field(() => ResolveDepartments)
+  department: ResolveDepartments
 }
+
+@ObjectType()
+export class ResolveDocuments extends OmitType(Document, [
+  'elaborator',
+  'revisor',
+  'approver',
+  'unit',
+  'department',
+] as const) {}
---
+++ src/documents/enums/documents.enums.ts
@@ -0,0 +1,9 @@
+export enum Status {
+  ELABORATION,
+  REVISION,
+  APPROVAL,
+  APPROVED,
+  REJECTED,
+  OBSOLETE,
+  INATIVE,
+}
