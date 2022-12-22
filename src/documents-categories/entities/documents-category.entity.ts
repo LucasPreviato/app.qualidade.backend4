ac8fdda6ee1eb1aa78195766327cfb9dbff6feb1
Lucas Previato <lucaspreviato@live.com>
2022-12-21 23:06:06 -0300

    feat(document): was implements document categories entity and enums

--- src/documents-categories/entities/documents-category.entity.ts
+++ src/documents-categories/entities/documents-category.entity.ts
@@ -1,7 +1,35 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql'
+import {
+  Field,
+  Int,
+  registerEnumType,
+  ObjectType,
+  OmitType,
+} from '@nestjs/graphql'
+import { CodeFormat } from '../enums/documents-categories.code-format.enums'
+
+registerEnumType(DocumentType, {
+  name: 'DocumentType',
+})
+registerEnumType(CodeFormat, {
+  name: 'CodeFormat',
+})
 
 @ObjectType()
 export class DocumentsCategory {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+  @Field(() => Int)
+  id: number
+  @Field(() => String)
+  name: string
+  @Field(() => Date)
+  createdAt: Date
+  @Field(() => DocumentType)
+  documentType: DocumentType
+  @Field(() => CodeFormat, { defaultValue: 'SIMPLE' })
+  codeFormat: CodeFormat
 }
+
+@ObjectType()
+export class ResolveDocumentsCategory extends OmitType(
+  DocumentsCategory,
+  [] as const
+) {}
