365a1046c995cfc211d8a9a56eeca0e5e5651824
Lucas Previato <lucaspreviato@live.com>
2022-12-30 21:55:55 -0300

    feat(document-category): was implemented all modulo documentcategory

--- src/documents-categories/documents-categories.module.ts
+++ src/documents-categories/documents-categories.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'
 import { DocumentsCategoriesService } from './documents-categories.service'
 import { DocumentsCategoriesResolver } from './documents-categories.resolver'
+import { DocumentsCategoriesRepository } from './repositories/documents-categories-repository'
+import { PrismaDocumentsCategoriesRepository } from './repositories/prisma/prisma-documents-categories-repository'
 
 @Module({
-  providers: [DocumentsCategoriesResolver, DocumentsCategoriesService],
+  providers: [
+    DocumentsCategoriesResolver,
+    DocumentsCategoriesService,
+    {
+      provide: DocumentsCategoriesRepository,
+      useClass: PrismaDocumentsCategoriesRepository,
+    },
+  ],
 })
 export class DocumentsCategoriesModule {}
