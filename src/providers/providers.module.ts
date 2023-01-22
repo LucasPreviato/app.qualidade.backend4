d0e55d786f9999aabf2ed59151910376b92e6e35
Lucas Previato <lucaspreviato@Live.com>
2023-01-22 17:14:19 -0300

    fix(providers): correction in the suppliers module that was not implementing the database
    inside module.ts was not importing the provider and the useclass

--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ProvidersService } from './providers.service';
 import { ProvidersResolver } from './providers.resolver';
+import { ProvidersRepository } from './repositories/providers.repository';
+import { PrismaProvidersRepository } from './repositories/prisma/prisma.providers.repository';
 
 @Module({
-  providers: [ProvidersResolver, ProvidersService],
+  providers: [
+    ProvidersResolver,
+    ProvidersService,
+    {
+      provide: ProvidersRepository,
+      useClass: PrismaProvidersRepository,
+    },
+  ],
 })
 export class ProvidersModule {}
